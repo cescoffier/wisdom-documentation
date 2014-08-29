1319ab9ce0e9cac20cb9a9909c43a2ef2514453d
jenf11 <jenfry11@gmail.com>
2014-08-29 10:21:01 +0200

    fixed small style issues

--- src/main/resources/assets/libs/wisdom-news.js
+++ src/main/resources/assets/libs/wisdom-news.js
@@ -1,46 +1,41 @@
 $(document).ready(function () {
     //load news list
-
     load();
-
 });
 
-
-
-
-
-/* list all available extension in a table */
-function writeExtensionData(data) {
-    alert("my friend the alert box!");
+/* list all available news */
+function writeNewsData(data) {
     $("#news-section").empty();
     $("<h1></h1>").addClass("highlight-color").html("News").appendTo("#news-section");
     $.each(data, function (index,value) {
-        console.log(index,value);
-
-        //create a new row using the extensions key as a collasable link
+        var newsId = value.id;
+        //replace orientdb id formatting for html tag compatibility
+        if (HasSubstring(newsId, "#")) {
+            newsId = newsId.replace("#", "-");
+        }
+        if (HasSubstring(newsId, ":")) {
+            newsId = newsId.replace(":", "-");
+        }
+        //create a new row using the news key as a collapsible link
         var article = $("<div></div>").addClass("article col-xs-12").appendTo("#news-section");
-        var title = $("<div></div>").addClass("title col-xs-12 col-sm-6").appendTo(article);
-        var date = $("<div></div>").addClass("date col-xs-12 col-sm-6").html(value.dateCreated).appendTo(article);
-        title.append($("<a></a>")
-            .attr("href", "#collapse" + value.title)
+        var title = $("<div></div>").addClass("title col-xs-6").appendTo(article);
+        var date = $("<div></div>").addClass("date col-xs-6").html(value.dateCreated).appendTo(article);
+        var icon = $("<i></i>").addClass("fa fa-leaf fa-fw leaf");
+        title.append($("<a>"+icon+"</a>")
+            .attr("href", "#collapse" + newsId)
             .attr("data-toggle", "collapse")
-            .html(value.title));
-
-        //create a list of the information for each key
-        var content = $("<div></div>").toggleClass("properties").addClass("articlecontent col-xs-12").html(value.content).appendTo(article);
-
-       // title.append(content);
-       // article.append(title).append(date).appendTo("news-section");
+            .html("&nbsp;"+value.title));
+        icon.prependTo(title);
+        //create a collapsible list of the information for each key
+        var content = $("<div></div>").attr("id","collapse"+newsId).addClass("articlecontent col-xs-12 collapse").html(value.content).appendTo(article);
 
         });
 
 }
 
-/*load the list of extensions as json */
+/*load the list of news as json */
 function load() {
-    console.log(window.location.host);
-    //$.get("http://" + window.location.host + "/registry/list").success(writeExtensionData)
-    $.get("http://localhost:9000/assets/newsstream.json").success(writeExtensionData);
+    $.get("http://localhost:9000/assets/newsstream.json").success(writeNewsData);
 }
 
 /**
